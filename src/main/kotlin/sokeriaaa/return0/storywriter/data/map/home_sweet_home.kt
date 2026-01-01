644b962faa63f40e4461126ae48fd063ca65e221
Sokeriaaa <73162265+sokeriaaa@users.noreply.github.com>
2026-01-01 09:47:07 +0800

    Continue home_sweet_home.kt

--- src/main/kotlin/sokeriaaa/return0/storywriter/data/map/home_sweet_home.kt
+++ src/main/kotlin/sokeriaaa/return0/storywriter/data/map/home_sweet_home.kt
@@ -1,9 +1,12 @@
 package sokeriaaa.return0.storywriter.data.map
 
+import sokeriaaa.return0.shared.data.api.component.value.Value
 import sokeriaaa.return0.shared.data.models.component.conditions.CommonCondition
 import sokeriaaa.return0.shared.data.models.story.event.Event
+import sokeriaaa.return0.shared.data.models.story.event.Event.TeleportUserTo
 import sokeriaaa.return0.shared.data.models.story.map.MapData
 import sokeriaaa.return0.shared.data.models.story.map.MapEvent
+import sokeriaaa.return0.storywriter.res.SWDialogueNames.unknown
 import sokeriaaa.return0.storywriter.utils.dialogueResCounter
 
 
@@ -39,6 +42,23 @@ private val homeEvent = MapEvent(
             narrator("The last piece of code that was not committed before being forcibly logged out still remained on it."),
             narrator("Your fingers hover above the keys, trembling slightly. The \"exit\" code still lingers in your mind, replaying every moment of Lawrence's words."),
             narrator("But as you stare at the blinking cursor, something inside you snaps."),
+
+            Event.TypeReturn0,
+            narrator("For a moment, nothing happens, but the circular progress indicator rotates."),
+            narrator("The room seems to grow stiller."),
+            narrator("Then, a sudden shift."),
+            Event.ShakeMap,
+            narrator("The terminal begins to pulse, expanding into a cascade of symbols and code — an endless matrix unfolding before your eyes."),
+            narrator("Your apartment walls seem to melt away, the very reality around you warping into fragments of syntax."),
+            narrator("The code grows larger, surrounding you in a digital cocoon."),
+            narrator("You feel yourself falling, but there’s no sensation of gravity."),
+            narrator("No body, no space. Only the flow of pure data around you — an overwhelming rush of endless lines of code, each command a path leading to something."),
+            narrator("The very fabric of your world has dissolved into a quantum stream of logic and mathematical equations."),
+            npc(unknown, "Welcome to the Grid, Ada Null."),
+            npc(unknown, "Your execution has begun."),
+
+            // Next scene
+            TeleportUserTo(SWMaps.main.name, Value(1))
         )
     },
 )
