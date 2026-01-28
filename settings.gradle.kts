79da2e3cf5caf5cd9c742a5e75b29bc7ea9ae083
Sokeriaaa <73162265+sokeriaaa@users.noreply.github.com>
2026-01-28 15:06:39 +0800

    Add refreshVersions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    // See https://splitties.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.6"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "Return0-storywriter"
